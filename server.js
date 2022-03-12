a5a84d9bdd347784498ab73994cce475a75a6cfd
Kerimcan <464639@student.fontys.nl>
2022-03-12 18:53:21 +0100

    mongoose + db connection via mongoose

--- server.js
+++ server.js
@@ -1,13 +1,27 @@
 const express = require('express')
 const app = express()
- 
+const mongoose = require('mongoose')
+
 app.get("/", (req,res) =>{
-    res.send('empty')
+    res.send("product list")
 })
 
+// routes
 const productRouter = require('./routes/products')
 
+
+// connect to DB
+mongoose.connect(
+    'mongodb://localhost:27017/products', 
+    { useNewUrlParser: true}, 
+    () => console.log('connect to db')
+)
+    
+
+// enabling routes
 app.use('/products',productRouter)
 
+
+// 
 app.listen(3000)
 
