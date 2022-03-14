2ceaa3b662290f8e9f56b8317c50d25856893632
Kerimcan <464639@student.fontys.nl>
2022-03-14 20:02:37 +0100

    Json acceptence + CRUD on Products

--- routes/products.js
+++ routes/products.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const Product = require('../models/product')
 
 
-// GET all products
+// GET all
 router.get("/", async (req,res) =>{
     try {
         const products = await Product.find()
@@ -13,17 +13,67 @@ router.get("/", async (req,res) =>{
     }
 })
 
-// GET product by id
-router.get("/:id", async (req,res) =>{
-    let id = req.params.id
+// GET by id
+router.get("/:id", getProduct ,async (req,res) =>{
+    res.json(res.product)
+})
 
+// POST 
+router.post('/', async (req,res) => {
+    const product = new Product({
+        title: req.body.title,
+        price: req.body.price
+    })
     try {
-        const products = await Product.findById(id)
-        res.json(products)
+        const newProduct = await product.save()
+        res.json(newProduct)
     } catch (error) {
-        res.json({message: "product not found, or is not available"})
+        res.json(error.message)
     }
 })
 
+
+// DELETE by id
+router.delete("/:id", getProduct, async (req,res) =>{
+    try {
+        await res.product.remove()
+        res.json({message: "Deleted product"})
+    } catch (error) {
+        res.json({message:"failed"})
+    }
+})
+
+// PATCH by id
+router.patch('/:id', getProduct, async (req,res) =>{
+    if (req.body.title != null){
+        res.product.title = req.body.title
+    }
+    if (req.body.price != null){
+        res.product.price = req.body.price
+    }
+
+    try {
+        const updatedProduct = await res.product.save()
+        res.json(updatedProduct)
+    } catch (error) {
+        res.json({message: error.message})
+    }
+})
+
+
+async function getProduct (req, res, next) {
+    let product
+    try {
+        product = await Product.findById(req.params.id) 
+        if (product == null){
+            return res.json({message: "Cannot find product"})
+        }
+    } catch (error) {
+        return res.json({message: error.message})
+    }
+
+    res.product = product
+    next()
+} 
 
 module.exports = router
--- server.js
+++ server.js
@@ -2,21 +2,25 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 require('dotenv/config')
-
 // routes
 const productRouter = require('./routes/products')
 
 // connect to DB
-mongoose.connect(
-    process.env.DB_CONNECTION, 
-    { useNewUrlParser: true}, 
-    () => console.log('connect to db')
-)
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true})
+
+const db = mongoose.connection
+db.on('error', (error)=> console.error(error))
+db.once('open', () => console.log("connected to db!"))
+
+
+app.use(express.json())
+
     
 // enabling routes
 app.use('/products',productRouter)
 
 
 // server port
-app.listen(3000)
+app.listen(3000, () => console.log('Server listening on port 3000'))
+
 
