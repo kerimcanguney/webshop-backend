27cc28934686822afd70c6721224334f10b1ab2b
Kerimcan <464639@student.fontys.nl>
2022-04-16 22:50:47 +0200

    jwt auth
    #5

--- Account-API/Account-API/Controllers/AccountController.cs
+++ Account-API/Account-API/Controllers/AccountController.cs
@@ -1,12 +1,12 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Account_API.Services;
 using Account_API.ViewModels;
 using Account_API.Models;
+using System.Security.Claims;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Account_API.Controllers
 {
@@ -14,15 +14,19 @@ namespace Account_API.Controllers
     [ApiController]
     public class AccountController : ControllerBase
     {
-        AccountService accountService = new AccountService();
-
+        private IConfiguration _config;
+        AccountService accountService;
+        public AccountController(IConfiguration _config)
+        {
+            this._config = _config;
+            accountService = new AccountService(_config);
+        }
         [HttpGet]
         [Route("/get")]
         public Array GetAllAccounts()
         {
             var context = new WsContext();
             var All = context.Accounts.ToArray();
-
             return All;
         }
         
@@ -40,5 +44,29 @@ namespace Account_API.Controllers
             return accountService.Login(Email, Password);
         }
 
+        [HttpGet]
+        [Authorize]
+        [Route("/Settings")]
+        public string AccountSettings()
+        {
+            Account account = GetCurrentUserViaHttpContext();
+            
+            return "Hello " + account.Surname;
+        }
+        private Account GetCurrentUserViaHttpContext()
+        {
+            var identity = HttpContext.User.Identity as ClaimsIdentity;
+
+            if (identity == null) return null;
+
+            var userClaims = identity.Claims;
+
+            return new Account
+            {
+                Surname = userClaims.FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier)?.Value,
+                Email = userClaims.FirstOrDefault(x => x.Type == ClaimTypes.Email)?.Value,
+
+            };
+        }
     }
 }
--- Account-API/Account-API/Startup.cs
+++ Account-API/Account-API/Startup.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -6,10 +7,12 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace Account_API
@@ -26,7 +29,19 @@ namespace Account_API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = Configuration["Jwt:Issuer"],
+                        ValidAudience = Configuration["Jwt:Audience"],
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Jwt:Key"]))
+                    }
+                );
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
@@ -47,7 +62,7 @@ namespace Account_API
             app.UseHttpsRedirection();
 
             app.UseRouting();
-
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
